248b0f5076c543520a646a25ec0589105d56db0c
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-10-04 01:30:00 +0100

    Add a simple DRF viewset

--- content_api/views.py
+++ content_api/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class TestViewSet(viewsets.ViewSet):
+    def list(self, request):
+        return Response('Success!')
