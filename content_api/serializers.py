9cb525d51c2ba18c746d0763979c0681328c0487
Max Goodridge <max.goodridge@hotmail.co.uk>
2018-01-26 00:13:30 +0000

    Add images to content items and serve during development

--- content_api/serializers.py
+++ content_api/serializers.py
@@ -6,4 +6,4 @@ from content_api.models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = 'id', 'title', 'description'
+        fields = 'id', 'title', 'description', 'image'
--- contentfeed/settings.py
+++ contentfeed/settings.py
@@ -111,4 +111,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'contentfeed')
+
 CORS_ORIGIN_WHITELIST = 'localhost:3000',
